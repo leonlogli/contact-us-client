534c8d69d945965501f66eba0d89df11c700ecd8
Léon Logli <leonlogli@gmail.com>
2019-08-26 02:31:36 +0100

    Initial setup
    Add bootstrap cdn and material ui package, 
    Add App root containers

---
+++ src/containers/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <div className="row justify-content-center">
+      <div className="col-md-7 text-center">
+        <h3 className="title my-3"><span>./</span>Contact</h3>
+        <h5><strong>engice</strong> encadre votre projet de A à Z</h5>
+        <p className="mt-2 mb-5">Le developpement sur mesure des sites web et d'applications mobiles
+        innovantes sont notre point fort. Du premier workshop à la maintenance,
+        nous développons des solutions pointues centrées sur lutilisateur et les
+        mettons en oeuvre efficacement à l'aide des méthodes agiles</p>
+      </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
